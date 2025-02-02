36d0d4081ad14ad9ad2b390a8456700a32f9dd05
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2025-02-02 16:28:04 +0200

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/m7mdselim.github.io/", // 👈 Add this
 })
