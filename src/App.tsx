21d56ce671baab7ae09edfd2176f3e7edc1fb05e
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2025-02-02 18:34:21 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from 'react';
+
+import { useState, useEffect } from 'react';
 
 import { Facebook, Instagram, Menu, X } from "lucide-react"; // Import icons if not already
 import { 
