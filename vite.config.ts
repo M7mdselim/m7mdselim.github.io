f2e242da765b9a4ab106890c7578082966f90425
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2025-02-02 03:06:35 +0200

    ..

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-})
+  base: '/MohammedSelim.com/', // Change this to match your repo name!
+});
