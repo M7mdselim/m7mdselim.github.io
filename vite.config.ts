cb6d85d802dfe7e22da2e54f8638b6cc3b28e001
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2025-02-02 18:13:05 +0200

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/m7mdselim.github.io/', // Ensure this has the trailing slash
+  base: '/', // Ensure this has the trailing slash
 })
