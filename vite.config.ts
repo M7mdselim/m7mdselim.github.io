e1389af24ec8fcd1260b245b76a498d28a283711
Mohammed Selim <57008993+M7mdselim@users.noreply.github.com>
2025-02-02 20:50:47 +0200

    Deploy Vite app to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/m7mdselim.github.io/', // This should match your GitHub Pages URL
+  base: '/m7mdselim.github.io/', // 👈 Add this line
 });
